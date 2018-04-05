107e0f6d12d9cb727c11ce91f8a24daed2d5f0fd
Mad <thuctran2217@gmail.com>
2018-04-05 16:35:54 +0700

    add pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravatar_image(user, option = {size: 80})
+        gravatar_id = Digest::MD5::hexdigest(user.email)
+        size = option[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, class: "img-circle")
+    end
 end
